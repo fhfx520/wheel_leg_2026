63b759f701d6cd4d55acda79c77d190fc4696432
fhfx520 <2178292159@qq.com>
2026-05-02 17:15:14 +0800

    前倾反飞

--- wheel_leg_2025--4/rm_main/User/Model/wlr.c
+++ wheel_leg_2025--4/rm_main/User/Model/wlr.c
@@ -703,7 +703,7 @@ static void handle_sky_state(void)
     if (wlr.sky_flag == WLR_SKY_FOLDING) {
         sky_leg_length = (wlr.double_flag ? 0.16f : 0.11f);
         wlr.high_set = ramp_calc(&sky_height_ramp, sky_leg_length);
-        x3_balance_zero = x3_balance_zero_normal ;
+        x3_balance_zero = x3_balance_zero_normal- 0.03f;
         x5_balance_zero = 0.0f;
         pid_leg_sky_jump[0].i_out = pid_leg_sky_jump[1].i_out = \
 	    pid_leg_sky_cover[0].i_out = pid_leg_sky_cover[1].i_out = \
@@ -733,8 +733,8 @@ static void handle_sky_state(void)
 		else
 		{
             wlr.v_ref = -2.5f;
-			x3_balance_zero = x3_balance_zero_normal + 0.1f;
-			x5_balance_zero = 0.0f;
+			x3_balance_zero = x3_balance_zero_normal;
+			x5_balance_zero = -0.02f;
 		}
         if (fabsf(0.30f - vmc[0].L_fdb) < 0.03f && fabsf(0.30f - vmc[1].L_fdb) < 0.03f) {
             wlr.sky_cnt++;
